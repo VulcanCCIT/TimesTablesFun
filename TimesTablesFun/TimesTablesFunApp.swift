f067997a35e9d6498ada39b4b360dbe954a6f421
VulcanCCIT <chuckcondron@chuckcondron.com>
2023-06-12 11:54:08 -0500

    Initial Commit

---
+++ TimesTablesFun/TimesTablesFunApp.swift
@@ -0,0 +1,17 @@
+//
+//  TimesTablesFunApp.swift
+//  TimesTablesFun
+//
+//  Created by Chuck Condron on 6/12/23.
+//
+
+import SwiftUI
+
+@main
+struct TimesTablesFunApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
